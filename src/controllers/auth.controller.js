007a4bd46526e82eac07a5ac221896b88a47e51e
PremVeerSingh <prem.veer2836@gmail.com>
2026-06-23 11:05:52 +0530

    fdsafds

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -41,7 +41,7 @@ export const register = async (req , res) => {
             resfreshToken,
             {
                 httpOnly: true,
-                secure: false,
+                secure: process.env.NODE_ENV === "production",
                 sameSite: "lax",
                 maxAge:
                     7 * 24 * 60 * 60 * 1000
