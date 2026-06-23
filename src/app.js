91de37e48f68541893defe38b6848a7d150e8eed
PremVeerSingh <prem.veer2836@gmail.com>
2026-06-23 13:25:56 +0530

    fdsaf

--- src/app.js
+++ src/app.js
@@ -22,6 +22,23 @@ app.get("/" , (req, res ) => {
     res.send("API running")
 })
 
+app.get("/health", async (req, res) => {
+    try {
+        await pool.query("SELECT 1");
+
+        res.json({
+            server: "running",
+            database: "connected",
+        });
+    } catch (error) {
+        res.status(500).json({
+            server: "running",
+            database: "disconnected",
+            error: error.message,
+        });
+    }
+});
+
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
